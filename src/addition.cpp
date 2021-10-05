7938cb614a6ea9e87cf7da49b343c1f148de13af
iLive <yogannk@gmail.com>
2021-10-05 20:29:33 +0530

    First Commit

---
+++ src/addition.cpp
@@ -0,0 +1,102 @@
+// Server side C/C++ program to demonstrate Socket programming
+#include <iostream>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string>
+#include <vector>
+
+
+int main(int argc, char const *argv[])
+{
+    int PORT = 0;
+    switch (argc)
+    {
+    case 1:
+        std::cerr<<"No command line arguments given";
+        exit(EXIT_FAILURE);
+        break;
+    case 2:
+        std::cerr<<"only port is given";
+        exit(EXIT_FAILURE);
+        break;
+    default:
+        PORT = std::stoi(argv[1]);
+        break;
+    }
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+    std::string hello;
+
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Forcefully attaching socket to the port 8080
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
+                   &opt, sizeof(opt)))
+    {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    // Forcefully attaching socket to the port 8080
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 3) < 0)
+    {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+    printf("waiting for server connection...\n");
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
+    {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+    if (!(valread = read(new_socket, buffer, 1024)))
+    {
+        perror("error reading");
+        exit(EXIT_FAILURE);
+    }
+
+    std::vector<std::string> temp = {""};
+    for (int i = 0; i < 1024; ++i)
+    {
+
+        switch (buffer[i])
+        {
+        case ' ':
+            temp.push_back("");
+            break;
+
+        default:
+            temp.back() = temp.back() + buffer[i];
+            break;
+        }
+    }
+
+    uint32_t sum = 0;
+    std::cout << "Message received: ";
+    for (auto i = temp.begin(); i != temp.end(); ++i)
+    {
+        std::cout << *i << ' ';
+        sum = sum + stoi(*i);
+    }
+    hello = std::to_string(sum);
+    std::cout << "\nMessage sent: " << hello << std::endl;
+    send(new_socket, hello.c_str(), hello.length(), 0);
+    return 0;
+}
---
+++ test/client_tester.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string>
+
+int main(int argc, char const *argv[])
+{
+    int port = 0;
+    switch (argc)
+    {
+    case 1:
+        perror("No command line arguments given");
+        exit(EXIT_FAILURE);
+        break;
+    case 2:
+        perror("only port is given");
+        exit(EXIT_FAILURE);
+        break;
+    default:
+        port = std::stoi(argv[1]);
+        std::cout << port << std::endl;
+        break;
+    }
+    std::cout << "number of command line arguments: " << argc << std::endl;
+    int sock = 0, valread;
+    struct sockaddr_in serv_addr;
+    std::string messageToSend = "";
+    for (int i = 2; i < argc; ++i)
+    {
+        messageToSend = messageToSend + argv[i];
+        if (i != argc - 1)
+        {
+            messageToSend = messageToSend + ' ';
+        }
+    }
+    char buffer[1024] = {0};
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(port);
+
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0)
+    {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+    send(sock, messageToSend.c_str(), messageToSend.length(), 0);
+    std::cout << "Message sent from client: " << messageToSend << std::endl;
+    valread = read(sock, buffer, 1024);
+    printf("%s\n", buffer);
+    return 0;
+}
